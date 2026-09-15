e4c923a8bc373500e51c9d3751dcfeea451b7fbd
Sky Distant <zhangtianyuan120220@gmail.com>
2026-09-15 17:16:27 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // 必须设置为 export 以生成静态资源
   images: {
-    unoptimized: true,
+    unoptimized: true, // 静态导出需禁用默认图片优化
   },
 };
 

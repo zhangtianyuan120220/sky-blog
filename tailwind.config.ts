4a14890b8553d2d863b73d3ff51b5a72039a7563
Sky Distant <zhangtianyuan120220@gmail.com>
2026-09-16 18:38:21 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",        // 扫描 src 目录 (app/ 页面等)
-    "./components/**/*.{js,ts,jsx,tsx,mdx}", // 必须！扫描根目录 components 文件夹
-    "./app/**/*.{js,ts,jsx,tsx,mdx}"
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};

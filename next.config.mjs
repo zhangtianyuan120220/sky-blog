6268406ebb809a050ea0eb72890e832c87c15679
Sky Distant <zhangtianyuan120220@gmail.com>
2026-09-16 18:52:09 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 开启静态导出，生成 out 文件夹供 Tauri 加载
+  output: 'export', // 静态导出，配合 Tauri 打包
   images: {
-    unoptimized: true, // 静态导出模式下必须禁用默认图片优化
+    unoptimized: true,
   },
 };
 

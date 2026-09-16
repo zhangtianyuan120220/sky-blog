c5ebdc6dda2ce913dc6961eafd8b88624d4ef660
Sky Distant <zhangtianyuan120220@gmail.com>
2026-09-16 20:07:49 +0800

    Create MarkdownViewer.tsx

---
+++ components/MarkdownViewer.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+interface MarkdownViewerProps {
+  content: string;
+}
+
+export const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ content }) => {
+  return (
+    <div className="prose prose-slate max-w-none dark:prose-invert leading-relaxed">
+      <ReactMarkdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default MarkdownViewer;
